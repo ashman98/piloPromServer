eec940e54c03c89828cf0d1b30e35c801509877b
Ashot Manukyan <50967132+ashman98@users.noreply.github.com>
2024-06-29 04:23:51 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 //
 //Route::get('/about',[\App\Http\Controllers\AboutController::class, 'index']);
 //
